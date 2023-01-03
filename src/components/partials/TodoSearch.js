fec081369af1891877801e3d21867f5bbcfaf3e6
Wiridho Partuaon Tambunan <wiridho@student.telkomuniversity.ac.id>
2023-01-03 15:26:55 +0700

    Fix css

--- src/components/partials/TodoSearch.js
+++ src/components/partials/TodoSearch.js
@@ -2,31 +2,29 @@ import { Link } from 'react-router-dom'
 import { FiSearch } from "react-icons/fi";
 
 const TodoSearch = ({ onSearch, keyword }) => {
-    return (
-        <>
-            <div className='row search_container'>
-                <h3>Todo Search</h3>
-                <div className='col-8 search_left'>
-                    <div className="input-group mb-3">
-                        <span className="input-group-text" id="basic-addon1"><FiSearch /></span>
-                        <input
-                            value={keyword}
-                            onChange={onSearch}
-                            className='form-control'
-                            type="text" placeholder='Search Todo...'
-                            aria-label="Username"
-                            aria-describedby="basic-addon1" />
-                    </div>
-                    <div className="d-grid gap-2">
-                        <button className="btn btn-primary" type="button">Search</button>
-                    </div>
-                </div>
-                <div className='col search_right'>
-                    <Link to='add' className="btn btn-primary">Add New Task</Link>
-                </div>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className='row search_container'>
+				<h3>Todo Search</h3>
+				<div className='col-8 search_left'>
+					<div className="input-group mb-3">
+						<span className="input-group-text" id="basic-addon1"><FiSearch /></span>
+						<input
+							value={keyword}
+							onChange={onSearch}
+							className='form-control'
+							type="text" placeholder='Search Todo...'
+							aria-label="Username"
+							aria-describedby="basic-addon1" />
+					</div>
+				</div>
+				<div className='search_button'>
+					<button className="btn btn-primary btn_search" type="button">Search</button>
+					<Link to='add' className="btn btn-primary">Add New Task</Link>
+				</div>
+			</div>
+		</>
+	)
 
 }
 
